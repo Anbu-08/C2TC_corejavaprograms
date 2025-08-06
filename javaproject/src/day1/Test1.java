80007fae7d0b75300d8fcd289741e5a49271cd62
ADMIN <ADMIN@DESKTOP-2LPON0P>
2025-08-06 21:57:55 +0530

    this is day one assignment

---
+++ javaproject/src/day1/Test1.java
@@ -0,0 +1,22 @@
+package day1;
+import java.util.*;
+import java.io.*;
+
+public class Test1 {
+
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner s = new Scanner(System.in);  // Unused, can be removed
+
+        System.out.println("Enter the values:");
+        int a, b, c;
+
+        a = Integer.parseInt(br.readLine());
+        b = Integer.parseInt(br.readLine());
+        c = a + b;
+
+        System.out.println("Sum is: " + c);
+
+	}
+	}
